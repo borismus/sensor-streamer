7869e8e1abbc3598f4e3ed2853ec18a3fba78291
Boris Smus <boris@smus.com>
2019-10-03 16:35:47 -0700

    labels

--- mobile/index.js
+++ mobile/index.js
@@ -3,8 +3,10 @@ window.addEventListener('devicemotion', onDeviceMotion, true);
 
 function onDeviceOrientation(e) {
   console.log('onDeviceOrientation', e);
+  document.querySelector('device-motion').innerHTML = JSON.stringify(e);
 }
 
 function onDeviceMotion(e) {
   console.log('onDeviceMotion', e);
+  document.querySelector('device-motion').innerHTML = JSON.stringify(e.rotationRate);
 }
