476114714e97bf6c96a06714bb50fb1fd0001a77
Boris Smus <boris@smus.com>
2019-10-04 15:06:35 -0700

    qr code stuff

--- desktop/index.js
+++ desktop/index.js
@@ -1,6 +1,7 @@
 import '../common/firebase.js';
 
 let channel;
+let isConnected = false;
 
 function onLoad() {
   const searchParams = new URLSearchParams(location.search);
@@ -9,9 +10,33 @@ function onLoad() {
   // Listen for data coming in on this channel.
   const path = `channel/${channel}`;
   firebase.database().ref(path).on('child_added', snap => onSensorData(snap.val()));
+
+  // Show a QR code to start.
+  showQRCode(getSensorSenderUrl());
+}
+
+function getSensorSenderUrl() {
+  let url = window.location.href;
+  url = url.replace('desktop', 'mobile');
+  url = url.replace('http://localhost:8000', 'https://borismus.github.io/sensor-streamer');
+  return url;
 }
 
 function onSensorData(sensorData) {
+  document.querySelector('#sensor-data').innerHTML = JSON.stringify(sensorData);
+}
+
+function showQRCode(url) {
+  const canvas = document.querySelector('#qr-code');
+  QRCode.toCanvas(canvas, url, (error) => {
+    if (error) {
+      console.error(`QR Code error`);
+    }
+  });
+}
+
+function hideQRCode() {
+  document.querySelector('#qr-code').innerHTML = '';
 }
 
 window.addEventListener('load', onLoad);
